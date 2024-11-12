28435cf5669628495f563ea2a969c0769de1d7ea
nekowawolf <irukainu20@gmail.com>
2024-11-12 22:35:41 +0700

    feat: add Getenv

--- db/connection.go
+++ db/connection.go
@@ -3,15 +3,28 @@ package db
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+
 )
 
 var dbPool *pgxpool.Pool
 
 func InitDB() error {
-	dbURL := "postgres://username:password@localhost:5432/dbname"
-	
+	if err := godotenv.Load(); err != nil {
+		return fmt.Errorf("failed to load .env file: %v", err)
+	}
+
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	dbURL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
+
 	config, err := pgxpool.ParseConfig(dbURL)
 	if err != nil {
 		return fmt.Errorf("error parsing db config: %v", err)
